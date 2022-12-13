82a287fa211eea01b929e99e1c2d2ed78422c9a6
Serhii Nosko <serhii.nosko@namecheap.com>
2022-12-13 12:20:00 +0200

    feat: fixed issues with mobile menu and added smooth navigation

--- src/menu.js
+++ src/menu.js
@@ -2,8 +2,14 @@ export const enableMobileMenu = () => {
     const menuButton = document.getElementById('mobile-nav-button');
     const closeButton = document.getElementById('close-button');
     const navList = document.getElementById('nav-list');
-    const navLinks = navLinks.querySelectorAll('a');
+    const navLinks = navList.querySelectorAll('a');
+    const desktopBreakpoint = window.matchMedia('(min-width: 859px)');
+    let isDesktop = desktopBreakpoint.matches;
     
+    desktopBreakpoint.addEventListener('change', () => {
+        isDesktop = desktopBreakpoint.matches;
+    });
+
     const handleClose = () => {
         navList.classList.remove('nav__list--visible');
         navList.classList.add('nav__list--hidden');
@@ -16,7 +22,16 @@ export const enableMobileMenu = () => {
     
     closeButton.addEventListener('click', handleClose);
     navLinks.forEach((link) => {
-        link.addEventListener('click', handleClose);
+        link.addEventListener('click', (e) => {
+            if (link.hash) {
+                e.preventDefault();
+                document.querySelector(link.hash).scrollIntoView({ block: 'start', behavior: 'smooth' });
+            }
+
+            if (!isDesktop) {
+                handleClose();
+            }
+        });
     });
 }
 
