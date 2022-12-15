cc1f354b6011c313f79bbb639abba40ca8de9d8b
blackrabbit99 <blackrabbit99@users.noreply.github.com>
2022-12-15 08:50:45 +0000

    deploy: 2fc73c72439ad1f012bc518351c9a7202388d578

---
+++ index.975ef6c8.js
@@ -0,0 +1,235 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+(function (modules, entry, mainEntry, parcelRequireName, globalName) {
+  /* eslint-disable no-undef */
+  var globalObject =
+    typeof globalThis !== 'undefined'
+      ? globalThis
+      : typeof self !== 'undefined'
+      ? self
+      : typeof window !== 'undefined'
+      ? window
+      : typeof global !== 'undefined'
+      ? global
+      : {};
+  /* eslint-enable no-undef */
+
+  // Save the require from previous bundle to this closure if any
+  var previousRequire =
+    typeof globalObject[parcelRequireName] === 'function' &&
+    globalObject[parcelRequireName];
+
+  var cache = previousRequire.cache || {};
+  // Do not use `require` to prevent Webpack from trying to bundle this call
+  var nodeRequire =
+    typeof module !== 'undefined' &&
+    typeof module.require === 'function' &&
+    module.require.bind(module);
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire =
+          typeof globalObject[parcelRequireName] === 'function' &&
+          globalObject[parcelRequireName];
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error("Cannot find module '" + name + "'");
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = (cache[name] = new newRequire.Module(name));
+
+      modules[name][0].call(
+        module.exports,
+        localRequire,
+        module,
+        module.exports,
+        this
+      );
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x) {
+      var res = localRequire.resolve(x);
+      return res === false ? {} : newRequire(res);
+    }
+
+    function resolve(x) {
+      var id = modules[name][1][x];
+      return id != null ? id : x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [
+      function (require, module) {
+        module.exports = exports;
+      },
+      {},
+    ];
+  };
+
+  Object.defineProperty(newRequire, 'root', {
+    get: function () {
+      return globalObject[parcelRequireName];
+    },
+  });
+
+  globalObject[parcelRequireName] = newRequire;
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  if (mainEntry) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(mainEntry);
+
+    // CommonJS
+    if (typeof exports === 'object' && typeof module !== 'undefined') {
+      module.exports = mainExports;
+
+      // RequireJS
+    } else if (typeof define === 'function' && define.amd) {
+      define(function () {
+        return mainExports;
+      });
+
+      // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+})({"8lqZg":[function(require,module,exports) {
+var _mainCss = require("./main.css");
+var _stickyNav = require("./stickyNav");
+var _menu = require("./menu");
+(0, _stickyNav.enableStickyNavigation)();
+(0, _menu.enableMobileMenu)();
+
+},{"./main.css":"5d8d2","./stickyNav":"6wjto","./menu":"frHky"}],"5d8d2":[function() {},{}],"6wjto":[function(require,module,exports) {
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+parcelHelpers.defineInteropFlag(exports);
+parcelHelpers.export(exports, "enableStickyNavigation", ()=>enableStickyNavigation);
+const enableStickyNavigation = ()=>{
+    const observer = new IntersectionObserver((entries)=>{
+        const { isIntersecting  } = entries[0];
+        const nav = document.getElementById("nav");
+        if (!isIntersecting) nav.classList.add("nav--sticky");
+        else nav.classList.remove("nav--sticky");
+    });
+    observer.observe(document.getElementById("header"));
+};
+
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}],"gkKU3":[function(require,module,exports) {
+exports.interopDefault = function(a) {
+    return a && a.__esModule ? a : {
+        default: a
+    };
+};
+exports.defineInteropFlag = function(a) {
+    Object.defineProperty(a, "__esModule", {
+        value: true
+    });
+};
+exports.exportAll = function(source, dest) {
+    Object.keys(source).forEach(function(key) {
+        if (key === "default" || key === "__esModule" || dest.hasOwnProperty(key)) return;
+        Object.defineProperty(dest, key, {
+            enumerable: true,
+            get: function() {
+                return source[key];
+            }
+        });
+    });
+    return dest;
+};
+exports.export = function(dest, destName, get) {
+    Object.defineProperty(dest, destName, {
+        enumerable: true,
+        get: get
+    });
+};
+
+},{}],"frHky":[function(require,module,exports) {
+var parcelHelpers = require("@parcel/transformer-js/src/esmodule-helpers.js");
+parcelHelpers.defineInteropFlag(exports);
+parcelHelpers.export(exports, "enableMobileMenu", ()=>enableMobileMenu);
+const enableMobileMenu = ()=>{
+    const menuButton = document.getElementById("mobile-nav-button");
+    const closeButton = document.getElementById("close-button");
+    const navList = document.getElementById("nav-list");
+    const navLinks = navList.querySelectorAll("a");
+    const desktopBreakpoint = window.matchMedia("(min-width: 859px)");
+    let isDesktop = desktopBreakpoint.matches;
+    desktopBreakpoint.addEventListener("change", ()=>{
+        isDesktop = desktopBreakpoint.matches;
+    });
+    const handleClose = ()=>{
+        navList.classList.remove("nav__list--visible");
+        navList.classList.add("nav__list--hidden");
+    };
+    menuButton.addEventListener("click", ()=>{
+        navList.classList.remove("nav__list--hidden");
+        navList.classList.add("nav__list--visible");
+    });
+    closeButton.addEventListener("click", handleClose);
+    navLinks.forEach((link)=>{
+        link.addEventListener("click", (e)=>{
+            if (link.hash) {
+                e.preventDefault();
+                document.querySelector(link.hash).scrollIntoView({
+                    block: "start",
+                    behavior: "smooth"
+                });
+            }
+            if (!isDesktop) handleClose();
+        });
+    });
+};
+
+},{"@parcel/transformer-js/src/esmodule-helpers.js":"gkKU3"}]},["8lqZg"], "8lqZg", "parcelRequire1c30")
+
+//# sourceMappingURL=index.975ef6c8.js.map
